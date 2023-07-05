848828fb3c74bb37676d6b7a2997671d7316fca1
Josias wilson <58275504+jowill836@users.noreply.github.com>
2023-07-05 08:28:08 +0000

    ss

--- valeursManquantes.py
+++ valeursManquantes.py
@@ -6,9 +6,12 @@ df = pd.read_csv('Capital_Bikeshare_dataD.csv')
 
 # Créer des valeurs manquantes
 num_missing_values = 100  # Nombre de valeurs manquantes à créer
-missing_indices = np.random.choice(df.index, size=num_missing_values, replace=False)  # Sélectionner des indices aléatoires
-
-df.loc[missing_indices, 'temp'] = np.nan  # Remplacer les valeurs aux indices sélectionnés par NaN
+rows = df.shape[0]
+cols = df.shape[1]
+missingIndices = np.random.choice(rows * cols, size=num_missing_values, replace=False)
+missing_rows = missingIndices // cols
+missing_cols = missingIndices % cols
+df.iloc[missing_rows, missing_cols] = np.nan
 
 # Enregistrer le nouveau jeu de données avec les valeurs manquantes
-df.to_csv('Capital_Bikeshare_dataDVV.csv', index=False)
+df.to_csv('Capital_Bikeshare_dataDD.csv', index=False)
