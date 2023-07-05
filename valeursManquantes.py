8fb4abd604747ed407fe0006c7976879906dc398
Josias wilson <58275504+jowill836@users.noreply.github.com>
2023-07-05 10:38:12 +0200

    Update valeursManquantes.py

--- valeursManquantes.py
+++ valeursManquantes.py
@@ -15,3 +15,17 @@ df.iloc[missing_rows, missing_cols] = np.nan
 
 # Enregistrer le nouveau jeu de données avec les valeurs manquantes
 df.to_csv('Capital_Bikeshare_dataDD.csv', index=False)
+
+
+import pandas as pd
+
+# Charger le fichier CSV
+df = pd.read_csv('fichier.csv')
+
+# Modifier le type des colonnes
+df['colonne1'] = df['colonne1'].astype(nouveau_type1)
+df['colonne2'] = df['colonne2'].astype(nouveau_type2)
+# Ajouter d'autres colonnes et types à modifier selon vos besoins
+
+# Enregistrer le nouveau jeu de données avec les types modifiés
+df.to_csv('fichier_modifie.csv', index=False)
